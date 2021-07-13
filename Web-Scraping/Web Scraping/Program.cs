8eddbde38724d1aedacfbf8f4896f1a3f7b71206
BLaboga <87369099+BLaboga@users.noreply.github.com>
2021-07-13 16:56:51 +0300

    Update Program.cs

--- Web-Scraping/Web Scraping/Program.cs
+++ Web-Scraping/Web Scraping/Program.cs
@@ -7,16 +7,16 @@ namespace Web_Scraping
     {
         static void Main(string[] args)
         {
-            var html = "https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos/q-.net/q-.net/vilniaus?page=1";
+            var html = "https://cv.lt/nuolatinis-darbas?department=1040&city=1010";
 
             HtmlWeb web = new HtmlWeb();
 
             var htmlDoc = web.Load(html);
-            var jobTitles = htmlDoc.DocumentNode.SelectNodes("//*[@class='offer_primary_info']");
+            var jobTitles = htmlDoc.DocumentNode.SelectNodes("//*[@class='info-wr']");
 
             foreach (var jobTitle in jobTitles)
             {   
-                Console.WriteLine(jobTitle.SelectSingleNode("h2").InnerText);
+                Console.WriteLine(jobTitle.SelectSingleNode("button").InnerText);
             }
 
 
