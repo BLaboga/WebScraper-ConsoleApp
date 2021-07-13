4fc6074ff5e58102ddcc31bfcac15ddd0fa0705f
BLaboga <87369099+BLaboga@users.noreply.github.com>
2021-07-13 15:10:47 +0300

    Add files via upload

---
+++ Web-Scraping/Web Scraping/Program.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace Web_Scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = "https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos/q-.net/q-.net/vilniaus?page=1";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+            var jobTitles = htmlDoc.DocumentNode.SelectNodes("//*[@class='offer_primary_info']");
+
+            foreach (var jobTitle in jobTitles)
+            {   
+                Console.WriteLine(jobTitle.SelectSingleNode("h2").InnerText);
+            }
+
+
+
+          
+        }
+    }
+}
